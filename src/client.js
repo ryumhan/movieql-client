f0a1c0bd67951a8995c89a77e18b583942bdf07f
ryumhan <ryum7han@gmail.com>
2022-08-03 19:28:29 +0900

    practice(gql setup) using gql, data could be fetched

---
+++ src/client.js
@@ -0,0 +1,19 @@
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      {
+        movies {
+          author
+        }
+      }
+    `,
+  })
+  .then((data) => console.log(data));
+export default client;
