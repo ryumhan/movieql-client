1a0236e1f7799f95f8df859c29b746fee0d6d132
ryumhan <ryum7han@gmail.com>
2022-08-09 22:54:17 +0900

    using ApolloProvider, client can fetch the data in all the components inside provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Movies from "./routes/Movies";
 import Movie from "./routes/Movie";
-import client from "./client";
 
 function App() {
   return (
--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,3 +1,30 @@
+import { useEffect, useState } from "react";
+import { useApolloClient, gql } from "@apollo/client";
+
 export default function Movies() {
-  return <div>This is a list of movie </div>;
+  const [movies, setMovies] = useState([]);
+  const client = useApolloClient();
+  useEffect(() => {
+    client
+      .query({
+        query: gql`
+          {
+            allMovies {
+              title
+              id
+            }
+          }
+        `,
+      })
+      .then((result) => setMovies(result.data.allMovies))
+      .catch((err) => console.error(err));
+  }, [client]);
+
+  return (
+    <ul>
+      {movies.map((movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </ul>
+  );
 }
