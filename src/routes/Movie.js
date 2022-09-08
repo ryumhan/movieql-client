ab9580aef0fa5005b3373f051c66e0b00c75b59e
ryum7han <ryum7han@gmail.com>
2022-09-08 15:00:34 +0900

    practice using UseParams, client can fetch not over fetched data.
    practice using UseParams, client can fetch not over fetched data.

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,3 +1,27 @@
+import { useParams } from "react-router-dom";
+import {gql, useQuery} from "@apollo/client";
+
+const GET_MOVIE = gql`
+  query getMovies($movieId : ID!){
+    movie(id:$movieId){
+    id
+    title
+    }
+  }
+`
+
 export default function Movie() {
-  return <div>This is movie detail</div>;
+  const params = useParams();
+  const {data, loading} = useQuery(GET_MOVIE, {
+    variables:{
+      movieId: params.id,
+    }
+    });
+  
+    console.log(data, loading);
+    if(loading){
+      return <h1>Fetching Movie...</h1>
+    }
+
+  return <div>{data.movie.title}</div>;
 }
--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,4 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
+import { Link } from 'react-router-dom'
 
 const ALL_MOVIES = gql`
   query getMovies {
@@ -29,7 +30,9 @@ export default function Movies() {
     <ul>
       <h1>Movies</h1>
       {data.allMovies.map((movie) => (
-        <li key={movie.id}>{movie.title}</li>
+        <li key={movie.id}>
+          <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
       ))}
       <h1>Movies</h1>
       {data.allTweets.map((tweet) => (
